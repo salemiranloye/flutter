5a26dd4f18d8845e2d9e0bed87f3df0ef1c6d55d
salemiranloye <salemiranloye@gmail.com>
2025-07-29 14:26:34 -0700

    Formatting

--- packages/flutter_tools/lib/src/web/devfs_proxy.dart
+++ packages/flutter_tools/lib/src/web/devfs_proxy.dart
@@ -38,7 +38,11 @@ abstract class ProxyRule {
           "Invalid regex pattern in replace 'regex': '$regex'. Treating $regex as string. Error: $e",
         );
       }
-      return RegexProxyRule(pattern: regexPattern, target: target, replacement: replacement?.trim());
+      return RegexProxyRule(
+        pattern: regexPattern,
+        target: target,
+        replacement: replacement?.trim(),
+      );
     } else {
       effectiveLogger.printError("'prefix' or 'regex' field must be provided");
       return null;
